8627be057c2737256d08c1101a2f5f9d53c3249b
Emanuel Viader <emanuel.viader@gmail.com>
2022-06-27 22:27:00 -0300

    desafio contador

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,12 +1,40 @@
 import './ItemListContainer.css';
 import React, { Fragment } from 'react';
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
+
 
 function ItemListContainer(props) {
+    const [num, setNum] = useState(0);
+
+    const sumar = () =>{
+      setNum(num + 1);
+    }
+
+    const restar= () =>{
+      if(num>0){
+      setNum(num - 1);
+      }
+    }
+
     return(
       <Fragment>
+        <div className='container'>
         <h2>{props.saludo}</h2>
+          <div className='row'>
+            <div className='col-md-3'>
+              <div className='card'>
+                <div className='card-body'>
+                    <h4 className='card-title'>Producto</h4>
+                    <p className='card-text text-secundary'>Contador</p>
+                    <a href='#' onClick={sumar} className='btn btn outline-secundary rounded-0' >+</a>
+                    <a href='#' onClick={restar} className='btn btn outline-secundary rounded-0' >-</a>
+                    <p >{num}</p>
+                </div>
+              </div>  
+            </div>
+          </div>
+        </div>
       </Fragment>
       );
   }
